40abb6ef615d8e3565b5c7673aba3282c69ff93a
raravel <youn@tree-some.dev>
2021-05-28 18:10:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# git-submodule-js
+
+This modules is git submodule file (.gitmodules) de/serializer in javascript.
+
+## Install
+
+```sh
+$ npm install git-submodule
+```
+
+## Usage
+
+### .gitmodules content
+```
+[submodule "DbConnector"]
+	path = DbConnector
+	url = https://github.com/chaconinc/DbConnector
+
+[submodule "DbConnector2"]
+	path = DbConnector2
+	url = https://github.com/chaconinc/DbConnector2
+```
+
+### String to JSON
+
+```typescript
+import fs from 'fs';
+const content = fs.readFileSync('.gitmodules', 'utf8');
+const result = deserialize(content);
+
+/*
+result:
+{
+	"DbConnector": {
+		"path": "DbConnector",
+		"url": "https://github.com/chaconinc/DbConnector"
+	},
+	"DbConnector2" {
+		"path": "DbConnector2",
+		"url": "https://github.com/chaconinc/DbConnector2"
+	}
+}
+*/
+```
+
+### JSON to String
+
+```typescript
+serialize(result);
+
+/*
+[submodule "DbConnector"]
+	path = DbConnector
+	url = https://github.com/chaconinc/DbConnector
+
+[submodule "DbConnector2"]
+	path = DbConnector2
+	url = https://github.com/chaconinc/DbConnector2
+*/
+```
+
+---
+
+Thanks.
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+export type Submodule = Record<string, Record<string, string>>;
+
+export function deserialize(str: string): Submodule {
+	const ss: string[] = str.split('\n');
+	const ret: Submodule = {};
+	for ( let idx=0; idx < ss.length; idx++ ) {
+		const line: string = ss[idx];
+		const result = line.match(/\[submodule\s+\"(.*?)\"\]/) as any;
+		if ( !result || result.length < 2 ) {
+			continue;
+		}
+		const submodule = result[1];
+		const obj: Record<string, string> = {};
+		if ( submodule ) {
+			while ( ss[++idx].trim() ) {
+				const subline = ss[idx].trim();
+				const [ subginal, key, value ] = subline.match(/(.*?)\s*=\s*(.*)/) as any;
+				if ( key && value ) {
+					obj[key] = value;
+				}
+			}
+			ret[submodule] = obj;
+		}
+	}
+	return ret;
+}
+
+export function serialize(obj: Submodule): string {
+	if ( !obj ) {
+		return '';
+	}
+
+	let str: string = '';
+
+	const entries = Object.entries(obj);
+	for ( const [ key, value ] of entries ) {
+		str += `[submodule "${key}"]\n`;
+		const values = Object.entries(value);
+		for ( const [ left, right ] of values ) {
+			str += `	${left} = ${right}\n`;
+		}
+		str += '\n';
+	}
+
+	return str;
+}
